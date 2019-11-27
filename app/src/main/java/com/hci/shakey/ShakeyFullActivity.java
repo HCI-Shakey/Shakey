dc8dd7d98e98fbbac4403b4a70c212b20f0f381f
ZhaohengLi <zhaohengli.thu@gmail.com>
2019-11-27 19:01:31 +0800

    Init.

---
+++ app/src/main/java/com/hci/shakey/ShakeyFullActivity.java
@@ -0,0 +1,14 @@
+package com.hci.shakey;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ShakeyFullActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shakey_full);
+    }
+}
