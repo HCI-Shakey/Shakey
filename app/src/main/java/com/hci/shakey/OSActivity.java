299689b846e1c75d3fd6e057c1f993b023e2ba88
ZhaohengLi <zhaohengli.thu@gmail.com>
2019-12-07 16:21:04 +0800

    添加了摇一摇动作的识别

--- app/src/main/java/com/hci/shakey/OSActivity.java
+++ app/src/main/java/com/hci/shakey/OSActivity.java
@@ -1,18 +1,26 @@
 package com.hci.shakey;
 
+import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.os.Vibrator;
 import android.view.View;
 import android.widget.Button;
 
 public class OSActivity extends AppCompatActivity {
+
+    boolean isShake;
+    SensorManager sensorManager;
+    ShakeMotionListener shakeMotionListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +30,10 @@ public class OSActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         setUpButtons();
+
+        isShake = false;
+        sensorManager = (SensorManager)getSystemService(Context.SENSOR_SERVICE);
+        shakeMotionListener = new ShakeMotionListener();
     }
 
     private void setUpButtons() {
@@ -75,4 +87,45 @@ public class OSActivity extends AppCompatActivity {
             }
         });
     }
+
+    private class ShakeMotionListener implements SensorEventListener {
+        @Override
+        public void onSensorChanged(SensorEvent event) {
+            if (isShake) return;
+            float x = Math.abs(event.values[0]);
+            float y = Math.abs(event.values[1]);
+            float z = Math.abs(event.values[2]);
+            if (x>19 || y>19 || z>19) {
+                isShake = true;
+                vibrate(500);
+                Intent intent = new Intent(OSActivity.this, ShakeyFullActivity.class);
+                startActivity(intent);
+                isShake = false;//可能会出现问题 重复启动shakey
+            }
+        }
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        //注册监听加速度传感器
+        sensorManager.registerListener(shakeMotionListener, sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_FASTEST);
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        //资源释放
+        sensorManager.unregisterListener(shakeMotionListener);
+        super.onPause();
+    }
+
+    private void vibrate(long milliseconds) {
+        Vibrator vibrator = (Vibrator)getSystemService(Service.VIBRATOR_SERVICE);
+        vibrator.vibrate(milliseconds);
+    }
+
+
 }
