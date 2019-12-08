9d38b6b86ddc7b58fa01f3daa5d7f9f795186e88
zhouxb17 <zhouxb17@mails.tsinghua.edu.cn>
2019-12-08 15:14:22 +0800

    add wechat scan and result

--- app/src/main/java/com/hci/shakey/WechatActivity.java
+++ app/src/main/java/com/hci/shakey/WechatActivity.java
@@ -16,6 +16,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.os.Vibrator;
+import android.content.ComponentName;
+import android.content.Intent;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
@@ -25,6 +27,9 @@ import android.widget.Button;
 import android.widget.PopupWindow;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import static android.content.Intent.FLAG_ACTIVITY_CLEAR_TOP;
+import static android.content.Intent.FLAG_RECEIVER_FOREGROUND;
 
 public class WechatActivity extends AppCompatActivity {
 
@@ -139,6 +144,7 @@ public class WechatActivity extends AppCompatActivity {
                 Toast.makeText(mContext, "button is pressed",Toast.LENGTH_SHORT).show();
                 //todo
                 shaking = false;
+                toWeChatScanDirect();
                 popupWindow.dismiss();
             }
         });
@@ -149,6 +155,7 @@ public class WechatActivity extends AppCompatActivity {
                 Toast.makeText(mContext, "button_init2 is pressed",Toast.LENGTH_SHORT).show();
                 //todo
                 shaking = false;
+                toWeChatDirect();
                 popupWindow.dismiss();
             }
         });
@@ -159,11 +166,46 @@ public class WechatActivity extends AppCompatActivity {
                 Toast.makeText(mContext, "button_init3 is pressed",Toast.LENGTH_SHORT).show();
                 //todo
                 shaking = false;
+                toWeChatDirect();
                 popupWindow.dismiss();
             }
         });
         popupWindow.showAsDropDown(view);
     }
+    //直接打开微信扫码界面
+    public void toWeChatScanDirect() {
+        try {
+            Intent intent = new Intent();
+            intent.setComponent(new ComponentName("com.tencent.mm", "com.tencent.mm.ui.LauncherUI"));
+            intent.putExtra("LauncherUI.From.Scaner.Shortcut", true);
+            intent.setFlags(FLAG_RECEIVER_FOREGROUND | FLAG_ACTIVITY_CLEAR_TOP);//335544320
+            intent.setAction("android.intent.action.VIEW");
+            startActivity(intent);
+            this.finish();
+        } catch (Exception e) {
+        }
+    }
+    //直接打开微信界面
+    public void  toWeChatDirect() {
+        try {
+            Intent intent = new Intent();
+            ComponentName cmp=new ComponentName("com.tencent.mm","com.tencent.mm.ui.LauncherUI");
+            intent.setAction(Intent.ACTION_MAIN);
+            intent.addCategory(Intent.CATEGORY_LAUNCHER);
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            intent.setComponent(cmp);
+            startActivity(intent);
+            this.finish();
+        } catch (Exception e) {
 
+        }
+    }
+    //直接打开虚假界面
+//    public void  toWeChatResult() {
+//        Intent intent = new Intent();
+//        intent.setClass(this, WeChatResultActivity.class);
+//        startActivity(intent);
+//        this.finish();
+//    }
 
 }
